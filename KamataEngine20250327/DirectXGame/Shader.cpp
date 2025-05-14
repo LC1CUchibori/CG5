4fab32baf7988d506608adb65718239fbe40e700
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2025-05-14 13:00:48 +0900

    22pまで

--- KamataEngine20250327/DirectXGame/Shader.cpp
+++ KamataEngine20250327/DirectXGame/Shader.cpp
@@ -1,18 +1,24 @@
 #include "Shader.h"
 #include <d3dcompiler.h>
 #include <cassert>
+#include "MiscUtility.h"
+#include <dxcapi.h>
+#pragma comment(lib,"dxcompiler.lib")
 
-void Shader::Load(const std::wstring& filePath, const std::string& shaderModel)
+void Shader::Load(const std::wstring& filePath, const std::wstring& shaderModel)
 {
 	ID3DBlob* shaderBlob = nullptr;
 	ID3DBlob* errorBlob = nullptr;
+
+	// wstring => string 文字列変換
+	std::string mbShaderModel = ConvertString(shaderModel);
 
 
 	HRESULT hr = D3DCompileFromFile(
 		filePath.c_str(), // シェーダーファイル名
 		nullptr,
 		D3D_COMPILE_STANDARD_FILE_INCLUDE,
-		"main", shaderModel.c_str(),
+		"main",  mbShaderModel.c_str(),
 		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
 		0, &shaderBlob, &errorBlob);
 
@@ -26,6 +32,55 @@ void Shader::Load(const std::wstring& filePath, const std::string& shaderModel)
 	}
 	// 生成したshaderBlobを取っておく
 	blob_ = shaderBlob;
+}
+
+void Shader::LoadDxc(const std::wstring& filePath, const std::wstring& shaderModel)
+{
+	static IDxcUtils* dxcUtils = nullptr;
+	static IDxcCompiler3* dxcCompiler = nullptr;
+	static IDxcIncludeHandler* includeHandler = nullptr;
+
+	HRESULT hr;
+
+	if (dxcUtils == nullptr) {
+		hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
+		assert(SUCCEEDED(hr)); // うまくいかなかったら止めて良い
+	}
+
+	if (dxcCompiler == nullptr) {
+		hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
+		assert(SUCCEEDED(hr));
+	}
+
+	if (includeHandler == nullptr) {
+		hr = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
+		assert(SUCCEEDED(hr));
+	}
+
+	// hlslファイルを読み込む
+    IDxcBlobEncoding* shaderSource = nullptr;
+	hr = dxcUtils->LoadFile(filePath.c_str(), nullptr, &shaderSource);
+	assert(SUCCEEDED(hr));
+
+	// 読み込んだファイルの内容を設定
+	DxcBuffer shaderSourceBuffer{};
+	shaderSourceBuffer.Ptr = shaderSource->GetBufferPointer();
+	shaderSourceBuffer.Size = shaderSource->GetBufferSize();
+	shaderSourceBuffer.Encoding = DXC_CP_UTF8;
+
+	// 2. Compileする
+	//    Compileに必要なコンパイルオプションの準備
+	LPCWSTR arguments[] = {
+		filePath.c_str(),
+		L"-E",
+		L"main",
+		L"-T",
+		shaderModel.c_str(),
+		L"-Zi",
+		L"-Qembed_debug",
+		L"-Od",
+		L"-Zpr",
+	};
 }
 
 ID3DBlob* Shader::GetBlob()
